43d78674fb26f388adc79015f2c614efbf571413
perick1 <erickfeliperez@gmail.com>
2021-07-13 17:44:56 -0400

    massive update

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,4 +7,5 @@ from utils._tablero import *#getBoard, plotBoard
 from utils._losetas import *#getPieces, plotMask
 from utils._caracteristicas import *
 from utils._match import *
+from utils._database import *
 #from utils.chess import ChessBoard
---
+++ utils/_database.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os, sys
+
+
+def getDatabase(path):
+    pass
